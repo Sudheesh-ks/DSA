403ac12d19230c4a9f8e4127e66f06ef630cdbe9
Sudheesh karippadath <sudheeshks132@gmail.com>
2025-07-18 12:09:55 +0530

    Update Recursion.js

--- Recursion/Recursion.js
+++ Recursion/Recursion.js
@@ -68,7 +68,7 @@
 
 // // // To reverse a string using recursion
 // // function reverseStr(str){
-// //     if(str === ""){
+// //     if(str.length <= 1){
 // //         return str;
 // //     }
 // //     return reverseStr(str.slice(1)) + str[0];
