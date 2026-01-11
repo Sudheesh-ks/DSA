f23912eac832d37ee9d07557fd7acb319c57e90a
Sudheesh ks <sudheeshks132@gmail.com>
2026-01-11 11:31:38 +0530

    Update hashTable.js

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -11,84 +11,6 @@
 //         for(let i = 0; i < key.length; i++){
 //             total += key.charCodeAt(i)
 //         }
-//         return total % this.size
-//     }
-
-//     set(key,value){
-//         const index = this.hash(key)
-//         // this.table[index] = value;
-//         const bucket = this.table[index]
-//         if(!bucket){
-//             this.table[index] = [[key,value]]
-//         }else{
-//             const sameKeyItem = bucket.find(item => item[0] === key)
-//             if(sameKeyItem){
-//                 sameKeyItem[1] = value
-//             }else{
-//                 bucket.push([key,value]);
-//             }
-//         }
-//     }
-
-//     get(key){
-//         const index = this.hash(key)
-//         // return this.table[index];
-//         const bucket = this.table[index]
-//         if(bucket){
-//             const sameKeyItem = bucket.find(item => item[0] === key)
-//             if(sameKeyItem){
-//                 return sameKeyItem[1];
-//             }
-//         }
-//     }
-
-//     remove(key){
-//         const  index = this.hash(key)
-//         // this.table[index] = undefined;
-//         const bucket = this.table[index]
-//         if(bucket){
-//             const  sameKeyItem = bucket.find(item => item[0] === key);
-//             if(sameKeyItem){
-//                 bucket.splice(bucket.indexOf(sameKeyItem[1]),1);
-//             }
-//         }
-//     }
-
-//     display(){
-//         for(let i = 0; i < this.table.length; i++){
-//             if(this.table[i]){
-//                 console.log(i, this.table[i]);
-//             }
-//         }
-//     }
-// }
-
-
-// const table = new HashTable(50)
-
-// table.set("name","Bruce");
-// table.set("age",38)
-// table.display();
-// console.log(table.get("name"));
-
-// table.remove("name");
-// table.display()
-
-
-// ------------- OPEN ADDRESSING ----------------------
-
-// === LINEAR PROBING ===
-// class HashTable {
-//     constructor(size){
-//         this.table = new Array(size);
-//         this.size = size;
-//     }
-
-//     hash(key){
-//         let total = 0;
-//         for(let i = 0; i < key.length; i++){
-//             total += key.charCodeAt(i)
-//         }
 //         return total % this.size;
 //     }
 
@@ -146,6 +68,12 @@
 // console.log(table.get("mane"));
 // table.remove("mane")
 // table.display()
+
+
+// ------------- OPEN ADDRESSING ----------------------
+
+// === LINEAR PROBING ===
+
 
 
 
