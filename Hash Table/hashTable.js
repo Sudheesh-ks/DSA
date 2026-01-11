7fe5522a4587240ddbfbbc74af66d86f12a07536
Sudheesh ks <sudheeshks132@gmail.com>
2026-01-11 12:13:13 +0530

    Update hashTable.js

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -138,7 +138,7 @@
 
 
 // ===QUADRATIC PROBING ===
-// class HashTableQuadraticProbing {
+// class HashTable {
 //     constructor(size){
 //         this.table = new Array(size);
 //         this.size = size;
@@ -147,60 +147,65 @@
 //     hash(key){
 //         let total = 0;
 //         for(let i = 0; i < key.length; i++){
-//             total += key.charCodeAt(i);
+//             total += key.charCodeAt(i)
 //         }
 //         return total % this.size;
 //     }
 
-//     set(key, value){
+//     set(key,value){
 //         let index = this.hash(key);
 //         let i = 1;
-
 //         while(this.table[index] && this.table[index][0] !== key){
 //             index = (this.hash(key) + i * i) % this.size;
 //             i++;
 //         }
-
-//         this.table[index] = [key, value];
+//         this.table[index] = [key,value];
 //     }
 
 //     get(key){
-//         let start = this.hash(key);
-//         let index = start;
+//         let index = this.hash(key);
 //         let i = 1;
-
-//         while(this.table[index]){
+//         while (this.table[index]) {
 //             if(this.table[index][0] === key){
 //                 return this.table[index][1];
 //             }
-//             index = (start + i * i) % this.size;
+//             index = (this.hash(key) + i * i) % this.size;
 //             i++;
 //         }
 //     }
 
 //     remove(key){
-//         let start = this.hash(key);
-//         let index = start;
+//         let index = this.hash(key);
+//         let deleted;
 //         let i = 1;
-
-//         while(this.table[index]){
-//             if(this.table[index][0] === key){
-//                 this.table[index] = undefined;
-//                 return;
+//         while (this.table[index]) {
+//             if(this.table[index] !== deleted && this.table[index][0] === key){
+//                 this.table[index] = deleted;
+//                 return
 //             }
-//             index = (start + i * i) % this.size;
+//             index = (this.hash(key) + i * i) % this.size;
 //             i++;
 //         }
 //     }
 
 //     display(){
-//         for(let i = 0; i < this.size; i++){
+//         for(let i = 0; i < this.table.length; i++){
 //             if(this.table[i]){
-//                 console.log(i, this.table[i]);
+//                 console.log(i, this.table[i])
 //             }
 //         }
 //     }
 // }
+
+
+// const table = new HashTable(50);
+// table.set("name","sudhi");
+// table.set("age",22)
+// table.set("mane",21)
+// table.set("amen", 3);
+// console.log(table.get("mane"));
+// table.remove("mane")
+// table.display()
 
 
 
