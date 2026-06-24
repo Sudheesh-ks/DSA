d41864e3252bfa6ff51e47296c1b145ec4ebeb92
Sudheesh ks <sudheeshks132@gmail.com>
2026-06-24 23:51:04 +0530

    Update hashTable.js

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -97,12 +97,10 @@
 
 //     get(key){
 //         let index = this.hash(key);
-//         while (this.table[index]) {
-//             if(this.table[index][0] === key){
-//                 return this.table[index][1];
-//             }
+//         while (this.table[index] && this.table[index][0] !== key) {
 //             index = (index + 1) % this.size;
 //         }
+//         return this.table[index][1];
 //     }
 
 //     remove(key){
