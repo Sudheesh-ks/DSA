01a14fe57515c3f1794cb984342309c476fb81b1
Sudheesh ks <sudheeshks132@gmail.com>
2026-07-24 19:09:49 +0530

    Update SubArrays.js

--- Arrays/SubArrays.js
+++ Arrays/SubArrays.js
@@ -132,3 +132,27 @@
 
 
 // console.log(subArray([1, 2, 3, 1, 2, 3, 4]))
+
+// Product except itself
+// function productExceptSelf(nums) {
+//     const n = nums.length;
+//     const result = new Array(n).fill(1);
+
+//     // Left products
+//     let left = 1;
+//     for (let i = 0; i < n; i++) {
+//         result[i] = left;
+//         left *= nums[i];
+//     }
+
+//     // Right products
+//     let right = 1;
+//     for (let i = n - 1; i >= 0; i--) {
+//         result[i] *= right;
+//         right *= nums[i];
+//     }
+
+//     return result;
+// }
+
+// console.log(productExceptSelf([1,2,3,4]));
