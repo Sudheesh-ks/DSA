86b07a4ea46ddd7c0841092bfd12099e67ab227d
Sudheesh ks <sudheeshks132@gmail.com>
2026-07-25 21:41:00 +0530

    Update SubArrays.js

--- Arrays/SubArrays.js
+++ Arrays/SubArrays.js
@@ -138,14 +138,12 @@
 //     const n = nums.length;
 //     const result = new Array(n).fill(1);
 
-//     // Left products
 //     let left = 1;
 //     for (let i = 0; i < n; i++) {
 //         result[i] = left;
 //         left *= nums[i];
 //     }
 
-//     // Right products
 //     let right = 1;
 //     for (let i = n - 1; i >= 0; i--) {
 //         result[i] *= right;
